7d9fcc7f3d3563022eaaa5170047337d21c68672
Chikki-21 <basavarajkchikki21@gmail.com>
2025-09-28 17:55:32 +0530

    Create setup.py

---
+++ packages/py_pkg/py_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robo',
+    maintainer_email='robo@todo.todo',
+    description='Simple example ROS 2 Python package (first_node).',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'first_node = py_pkg.first_node:main'
+        ],
+    },
+)
