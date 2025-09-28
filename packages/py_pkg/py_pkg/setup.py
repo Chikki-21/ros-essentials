76b51e89326d632a72ce69683e33de692ecb7a09
Chikki-21 <basavarajkchikki21@gmail.com>
2025-09-28 18:16:11 +0530

    change executable name

--- packages/py_pkg/py_pkg/setup.py
+++ packages/py_pkg/py_pkg/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'first_node = py_pkg.first_node:main'
+            'first_node = py_pkg.py_node:main'
         ],
     },
 )
